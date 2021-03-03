d0e81ffb6c2e0600b6ef61524f09dd038f726fc5
jxch <15553598935@163.com>
2021-03-03 17:17:19 +0800

    LRU 算法测试

---
+++ src/test/java/org/jxch/study/util/cache/LRUCacheTest.java
@@ -0,0 +1,41 @@
+package org.jxch.study.util.cache;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class LRUCacheTest {
+    private static final LRUCache<Integer> cache;
+
+    static {
+        cache = new LRUCache<>(5);
+        cache.put(1);
+        cache.put(2);
+        cache.put(3);
+        cache.put(4);
+        cache.put(5);
+    }
+
+    @Test
+    void put() {
+        cache.put(0);
+        logCache();
+    }
+
+    @Test
+    void get() {
+        cache.get(1);
+        logCache();
+    }
+
+    @Test
+    void findAndGet() {
+        cache.findAndGet(integer -> integer.equals(3));
+        logCache();
+    }
+
+    void logCache() {
+        log.info("cache:" + JSON.toJSONString(cache));
+    }
+}
